61e64c571f842d506ccf9668a4a1af8d57f06fbd
Peter Tran <pltran875@gmail.com>
2021-05-22 00:18:51 +0100

    Comparators and Comparable

---
+++ app/src/main/java/com/plt875/country/CountrySpeakersComparator.java
@@ -0,0 +1,12 @@
+package com.plt875.country;
+
+import java.util.Comparator;
+
+public class CountrySpeakersComparable implements Comparator<CountryLanguage> {
+
+    @Override
+    public int compare(CountryLanguage cl0, CountryLanguage cl1) {
+        return cl0.getSpeakers() - cl1.getSpeakers();
+    }
+
+}
